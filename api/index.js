79abfd54c03fec89b2e8a5d07dae8e74ab3ddb3f
Dovidas <Dovidas.Orechovas@stud.vtmc.lt>
2023-04-18 13:06:14 +0300

    3# commit: Logout jau turi funkcija, daroma create post forma

--- api/index.js
+++ api/index.js
@@ -5,12 +5,14 @@ const User = require('./models/User');
 const bcrypt = require('bcryptjs');
 const app = express();
 const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
 
 const salt = bcrypt.genSaltSync(10);
 const secret = 'afsbdafgdgdfsg';
 
 app.use(cors({credentials:true,origin:'http://localhost:3000'}));
 app.use(express.json());
+app.use(cookieParser());
 
 mongoose.connect('mongodb+srv://blog:EUOQBwEukMlebmOr@cluster0.fxcwq9l.mongodb.net/?retryWrites=true&w=majority');
 
@@ -34,14 +36,29 @@ app.post('/login', async (req,res) => {
     const passOk = bcrypt.compareSync(password, userDoc.password);
     if (passOk) {
       // logged in 
-      jwt.sign({username, id:userDoc. id}, secret, {}, (err,token) => {
+      jwt.sign({username, id:userDoc._id}, secret, {}, (err,token) => {
         if (err) throw err;
-        res.cookie('token', token).json('ok');
+        res.cookie('token', token).json({
+          id:userDoc._id,
+          username,
+        });
       });
       // res.json();
     } else {
         res.status(400).json('wrong credentials');
     }
 });
+
+app.get('/profile', (req,res) => {
+    const {token} = req.cookies;
+    jwt.verify(token, secret, {}, (err,info) => {
+      if (err) throw err;
+      res.json(info);
+    });
+});
+
+app.post('/logout', (req,res) => {
+  res.cookie('token', '').json('ok');
+})
 
 app.listen(4000);
