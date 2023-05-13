74ea101735d46b94a053aea2474de5360c0532b3
Dovidas <Dovidas.Orechovas@stud.vtmc.lt>
2023-05-13 22:31:36 +0300

    baigta

--- client/src/pages/CreatePost.js
+++ client/src/pages/CreatePost.js
@@ -2,22 +2,7 @@ import ReactQuill from "react-quill";
 import "react-quill/dist/quill.snow.css";
 import {useState} from "react";
 import {Navigate} from "react-router-dom";
-
-const modules = {
-    toolbar: [
-        [{ 'header': [1,2, false] }],
-        ['bold', 'italic', 'underline', 'strike', 'blockquote'],
-        [{'list': 'ordered'}, {'list': 'bullet'}, {'indent': '-1'}, {'indent': +1}],
-        ['link', 'image'],
-        ['clean']
-    ]
-};
-const formats = [
-    'header',
-    'bold', 'italic', 'underline', 'strike', 'blockquote',
-    'list', 'bullet', 'indent',
-    'link', 'image'
-];
+import Editor from "../Editor";
 
 export default function CreatePost() {
     const [title,setTitle] = useState('');
@@ -57,11 +42,7 @@ export default function CreatePost() {
                    onChange={ev => setSummary(ev.target.value)} />
             <input type="file" 
                    onChange={ev => setFile(ev.target.files)} />
-            <ReactQuill 
-            value={content} 
-            onChange={newValue => setContent(newValue)} 
-            modules={modules} 
-            formats={formats} />
+            <Editor value={content} onChange={setContent} />
             <button style={{marginTop:'5px'}}>Create post</button>
         </form>
     );
